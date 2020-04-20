731ed923c1099da1c3ecad844ffc126f7935e600
Terry <jtchoi@aitrics.com>
2020-04-20 12:09:12 +0900

    Build index.js

--- dist/index.js
+++ dist/index.js
@@ -99,23 +99,23 @@ try {
     const publickKey = core.getInput('PUBLICKEY');
     const fileUrl = core.getInput('FILE_URL');
     const platform = core.getInput('PLATFORM');
-    console.log(token);
-    Object(node_fetch__WEBPACK_IMPORTED_MODULE_0__["default"])({
-        method: 'POST',
-        url: `https://${token}@api.appetize.io/v1/apps/${publickKey}`,
-        headers: {
-            contentType: 'application/json',
-            Accept: 'application/json, text/plain, */*',
-        },
-        body: JSON.stringify({
-            url: fileUrl,
-            platform: platform
-        })
-    }).then(response => {
-        // core.setOutput("time", time);
-    }).catch(error => {
-        core.setFailed(error.message);
-    });
+    Object(node_fetch__WEBPACK_IMPORTED_MODULE_0__["default"])(
+        `https://${token}@api.appetize.io/v1/apps/${publickKey}`,
+        {
+            method: 'POST',
+            headers: {
+                contentType: 'application/json',
+                Accept: 'application/json, text/plain, */*',
+            },
+            body: JSON.stringify({
+                url: fileUrl,
+                platform: platform
+            })
+        }).then(response => {
+            // core.setOutput("time", time);
+        }).catch(error => {
+            core.setFailed(error.message);
+        });
 } catch (error) {
     core.setFailed(error.message);
 }
