ec73e04190273dc342ce980deacb3eb792b8b910
Terry <jtchoi@aitrics.com>
2020-04-20 11:22:21 +0900

    Fix a bug

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ try {
     const publickKey = core.getInput('PUBLICKEY');
     const fileUrl = core.getInput('FILE_URL');
     const platform = core.getInput('PLATFORM');
-    fetch({
-        method: 'POST',
-        url: `https://${token}@api.appetize.io/v1/apps/${publickKey}`,
-        headers: {
-            contentType: 'application/json',
-            Accept: 'application/json, text/plain, */*',
-        },
-        body: JSON.stringify({
-            url: fileUrl,
-            platform: platform
-        })
-    }).then(response => {
-        // core.setOutput("time", time);
-    }).catch(error => {
-        core.setFailed(error.message);
-    });
+    fetch(
+        `https://${token}@api.appetize.io/v1/apps/${publickKey}`,
+        {
+            method: 'POST',
+            headers: {
+                contentType: 'application/json',
+                Accept: 'application/json, text/plain, */*',
+            },
+            body: JSON.stringify({
+                url: fileUrl,
+                platform: platform
+            })
+        }).then(response => {
+            // core.setOutput("time", time);
+        }).catch(error => {
+            core.setFailed(error.message);
+        });
 } catch (error) {
     core.setFailed(error.message);
 }
